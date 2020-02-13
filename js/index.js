547eaf626fb438b799dcf6fb4c8436c315000ef3
kelebro666 <kelebro666@gmail.com>
2020-02-13 21:05:03 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,47 +1,46 @@
 const display = document.querySelector('.display');
+    let isResultRecoded = false;
+    let isOperButtonPressed = false;
 
 document.querySelectorAll('.digits button')
     .forEach(button => button.addEventListener('click', digitPressed));
 
 function digitPressed(ev) {
+    if(isResultRecoded && !isOperButtonPressed) clearResults();
     display.value += ev.target.innerText;
 }
 document.querySelectorAll('.opers button')
     .forEach(button => button.addEventListener('click', opersPressed));
 
 function opersPressed(ev) {
-    display.value += ev.target.innerText;
-}
+    if (display.value.slice(-1) === '+' ||
+     display.value.slice(-1) === '-' || 
+     display.value.slice(-1) === '*' || 
+     display.value.slice(-1) === '/') {
+         display.value = display.value.substring(0, display.value.length - 1) + ev.target.innerText;
+    } else {
+         display.value += ev.target.innerText;
+    }
+} 
+
 document.querySelector('.eq').addEventListener('click', calculate);
     function calculate() {
-        display.value = eval(display.value);
+        if (display.value.slice(-1) === '+' || display.value.slice(-1) === '*' || display.value.slice(-1) === '/' || display.value.slice(-1) === '-') display.value = display.value.substring(0, display.value.length - 1);
+    (eval(display.value) === Infinity || eval(display.value) === -Infinity) ? display.value = 'You can\'t divide by zero!'
+        : display.value = Math.round((eval(display.value) + Number.EPSILON) * 100) / 100;
+}
+
+document.querySelector('.clearAll').addEventListener('click', clearResults);
+    function clearResults() {
+        display.value = '';
     }
 
-// textArea = document.getElementById('display');
-//     function onButtonClick(e) {
-//         // e - MouseEvent (содержит информацию о клике)
-//         if (e.target.innerText === 'c') {
-//             // Если нажата кнопка "с", то стирает все из текстового поля
-//             textArea.innerText = '0';
-//         }
-
-
-// document.querySelector('.clear').addEventListener('click', clears);
-//     function clears() {
-//       if "C" = display.value = "0";
-//     }
-
-// // document.querySelector('.decimal').addEventListener('.click', decimalAdd)
-// //     function decimalAdd()  {
-// //   display.textContent = "1" + '.'
-// // }
-
-const input = document.querySelector('.display');
-        let inputValues = input.innerText;
-        let buttonValues = this.innerText; //this will refer to button Values
-
-        //the clear button.....clears the calculator
-        if (buttonValues == 'clear') {
-          input.innerText = '';
-          onlyDecimal = false;
-        }
+document.querySelector('.clear').addEventListener('click', clearOne);
+    function clearOne() {
+        display.value = display.value.substring(0, display.value.length - 1);
+}
+    
+document.querySelector('.decimal').addEventListener('click', createDecimal);
+    function createDecimal(ev) {
+        if (display.value.slice(-1) !== '.') display.value += ev.target.innerText;
+}
